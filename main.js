ddfb673eb74041782876899d1b0447f5ddcf3145
Aviraj19 <73528868+Aviraj19@users.noreply.github.com>
2021-08-19 16:35:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,16 +9,33 @@ console.log(event);
 var content=event.results[0][0].transcript;
 console.log(content);
 document.getElementById("textbox").innerHTML=content;
-speak();
+if (content=="take my selfie") {
+    speak();   
+}
 }
 function speak() {
     var synth = window.speechSynthesis;
-    speakdata=document.getElementById("textbox").value
+    speakdata="taking new selfie in 5 seconds";
     var utterance= new SpeechSynthesisUtterance(speakdata);
     synth.speak(utterance);
     Webcam.attach(camera);
+    setTimeout(function(){
+  take_snapshot() ;
+  save()
+    },5000);
 }
 Webcam.set({
 width:360,height:250,image_format:"jpeg",jpeg_quality:90
 });
 camera=document.getElementById("camera");
+function take_snapshot() {
+  Webcam.snap(function(data_url){
+document.getElementById("result").innerHTML="<img id='img1' src='"+data_url+"'>";
+  });
+}
+function save() {
+    link=document.getElementById("link")
+    img=document.getElementById("img1").src;
+    link.href=img;
+    link.click();
+}
